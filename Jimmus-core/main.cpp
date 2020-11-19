062b552b329d0d862bc310c27eeafab3d2d6ad64
GTucker2 <griffinavtucker@gmail.com>
2020-11-19 00:03:39 -0600

    GLEW library integration.

--- Jimmus-core/main.cpp
+++ Jimmus-core/main.cpp
@@ -9,12 +9,20 @@ int main() {
 	Window window("Jimmus", 960, 640);
 	glClearColor(0.2f, 0.3f, 0.8f, 0.1f);
 
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
 	while (!window.closed()) {
 		// use to check width and height
 		// std::cout << window.getWidth() << ", " << window.getHeight() << std::endl;
 		window.clear();
+
+		// This does nothing other than verify that GLEW works.
+		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+		
 		window.update();
 	}
 
--- Jimmus-core/src/graphics/window.cpp
+++ Jimmus-core/src/graphics/window.cpp
@@ -25,12 +25,19 @@ namespace jimmus {	namespace graphics {
 
 		m_Window = glfwCreateWindow(m_Width, m_Height, m_Title, NULL, NULL);
 		if (!m_Window) {
-			std::cout << "Failed to created GLFW Window!" << std::endl;
+			std::cout << "Failed to create GLFW Window!" << std::endl;
 			return false;
 		}
 		
 		glfwMakeContextCurrent(m_Window);
 		glfwSetWindowSizeCallback(m_Window, windowResize);
+		
+		std::cout << "OpenGL " << glGetString(GL_VERSION) << std::endl;
+
+		if (glewInit() != GLEW_OK) {
+			std::cout << "Could not initialize GLEW!" << std::endl;
+			return false;
+		}
 		return true;
 	}
 
